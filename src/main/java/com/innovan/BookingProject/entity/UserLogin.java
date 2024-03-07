d0322b7e439ddc9ec91c25b1e0e8477bbe4d11df
Ashish12100 <Puppalaashish12@gmail.com>
2024-03-07 12:07:36 +0530

    Added entity class userLogin

---
+++ src/main/java/com/innovan/BookingProject/entity/UserLogin.java
@@ -0,0 +1,18 @@
+package com.innovan.BookingProject.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "LoginDetails")
+public class UserLogin {
+    @MongoId(FieldType.OBJECT_ID)
+    private String Id;
+    private String userName;
+    private String email;
+    private Long phone;
+    private String password;
+
+
+}
