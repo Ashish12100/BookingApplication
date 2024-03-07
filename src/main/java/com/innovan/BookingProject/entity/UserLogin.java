d37b0a7a73f1de557347abb2ad63c8951795389a
Ashish12100 <Puppalaashish12@gmail.com>
2024-03-07 13:10:43 +0530

    added getters and setters in entity class

--- src/main/java/com/innovan/BookingProject/entity/UserLogin.java
+++ src/main/java/com/innovan/BookingProject/entity/UserLogin.java
@@ -1,15 +1,11 @@
 package com.innovan.BookingProject.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Document(collection = "LoginDetails")
 public class UserLogin {
     @MongoId(FieldType.OBJECT_ID)
@@ -19,5 +15,54 @@ public class UserLogin {
     private Long phone;
     private String password;
 
+    public String getId() {
+        return Id;
+    }
 
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserLogin(String id, String username, String email, Long phone, String password) {
+        Id = id;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public UserLogin() {
+    }
 }
