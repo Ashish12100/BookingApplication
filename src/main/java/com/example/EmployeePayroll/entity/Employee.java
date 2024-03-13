79e6c0998897c3a355fa0b21f2fd0b72b38ae057
Sankar I <idadasusankar1819@gmail.com>
2024-03-13 14:53:06 +0530

    Revert "added some changes"
    This reverts commit a8a730a54d1475cf6185e5f7abb132629b05d892.

--- src/main/java/com/example/EmployeePayroll/entity/Employee.java
+++ src/main/java/com/example/EmployeePayroll/entity/Employee.java
@@ -1,13 +1,9 @@
 package com.example.EmployeePayroll.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import org.springframework.stereotype.Component;
 
 @Component
-@Entity
 public class Employee {
-    @Id
     private int empId;
     private String  name;
     private long number;
