902819ac4ff96634ea6622e26d5f333f24f8ab7b
Sankar I <idadasusankar1819@gmail.com>
2024-03-13 14:19:42 +0530

    entity classes added

---
+++ src/main/java/com/example/EmployeePayroll/entity/Employee.java
@@ -0,0 +1,63 @@
+package com.example.EmployeePayroll.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    private int empId;
+    private String  name;
+    private long number;
+    private String address;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(int empId, String name, long number, String address) {
+        this.empId = empId;
+        this.name = name;
+        this.number = number;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", name='" + name + '\'' +
+                ", salary=" + number +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/EmployeePayroll/entity/Payroll.java
@@ -0,0 +1,5 @@
+package com.example.EmployeePayroll.entity;
+
+public class Payroll {
+
+}
